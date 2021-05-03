0729e3e1a52ccb5a03d94d1e0b7d9e495203912c
Amaan Ali <>
2021-05-03 03:57:31 +0000

    Finished all the exercises

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,37 @@
 package gr11review.part1;
+
+public class Review8 {
+
+    public static void main(String[] args) {
+
+        int MaxNumber = 8;
+        int MinNumber = 1;
+        int Range = MaxNumber - Min + 1;
+        int pick1;
+        int pick2;
+        int pick3;
+        int tripleNumbers = 0;
+
+
+        for (int i =0; i < 1000; i ++){
+
+            pick1 = (int) (Math.random() * Range) + MinNumber;
+            pick2 = (int) (Math.random() * Range) + MinNumber;
+            pick3 = (int) (Math.random() * Range) + MinNumber;
+            System.out.print(pick1 +" ");
+            System.out.print(pick2 +" ");
+            System.out.println(pick3);
+            if(pick1 == pick2 && pick1 == pick3){
+                tripleNumbers ++;
+
+            }
+
+        }
+
+        System.out.println(tripleNumbers);
+
+
+
+
+    }
+}
